6ffc4e4caac8dd3bf08397234fd58e43d7cc44a2
Lucas Lindh <lucas.lindh6@live.se>
2025-12-02 16:04:44 +0100

    task 1 continue

--- sql/eda_sakila.sql
+++ sql/eda_sakila.sql
@@ -7,3 +7,54 @@ FROM
     film
 WHERE
     length > 180;
+
+-- b)
+SELECT
+    title,
+    rating,
+    length,
+    description
+FROM
+    film
+WHERE
+    LIKE 'love' IN title;
+
+-- c)
+SELECT
+    title,
+    length
+FROM
+    film
+WHERE
+    length <= 46
+ORDER BY
+    length;
+
+SELECT
+    AVG(length) AS average_length,
+    MEDIAN (length) AS median_length
+FROM
+    film;
+
+SELECT
+    title,
+    length
+FROM
+    film
+WHERE
+    length >= 185
+ORDER BY
+    length DESC;
+
+-- d)
+SELECT
+    title,
+    rental_rate,
+    rental_duration,
+    rental_rate / rental_duration AS cost_per_day
+FROM
+    film
+ORDER BY
+    cost_per_day DESC
+LIMIT
+    10;

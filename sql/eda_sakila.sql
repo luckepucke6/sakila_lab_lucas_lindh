9d165ce31e73b6e4b91f020f2db9fa24074bb8e8
Lucas Lindh <lucas.lindh6@live.se>
2025-12-04 16:52:52 +0100

    f) start

--- sql/eda_sakila.sql
+++ sql/eda_sakila.sql
@@ -67,3 +67,39 @@ FROM
     actor a
 JOIN
     film_actor fa ON a.actor_id = fa.actor_id;
+
+-- f)
+-- 1. Vilken film har hyrts flest gånger?
+SELECT
+    f.title,
+    COUNT(r.rental_id) AS sum_rental
+FROM
+    film f
+JOIN inventory i ON i.film_id = f.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY f.film_id, f.title
+ORDER BY sum_rental DESC
+LIMIT 10;
+
+
+-- 2. Vilka kategorier hyr flest gånger?
+SELECT
+    c.name,
+    COUNT(r.rental_id) AS sum_rental
+FROM
+    category c
+JOIN film_category fc ON fc.category_id = c.category_id
+JOIN film f ON f.film_id = fc.film_id
+JOIN inventory i ON i.film_id = f.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+JOIN customer cu ON cu.customer_id = r.customer_id
+GROUP BY c.category_id, c.name
+ORDER BY sum_rental DESC;
+
+
+
+
+
+-- 3. Hur många hyrningar i snitt görs varje månad?
+-- 4. Vilket land hyr flest filmer?
+-- 5. Vilka städer har flest kunder?

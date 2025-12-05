8895ef9edce5c7d0cb764b6e31315ac629b5555b
Lucas Lindh <lucas.lindh6@live.se>
2025-12-05 15:30:43 +0100

    f) done

--- sql/eda_sakila.sql
+++ sql/eda_sakila.sql
@@ -125,3 +125,16 @@ LIMIT 10;
 
 
 -- 5. Vilka städer har flest kunder?
+SELECT
+    city,
+    country,
+    COUNT(customer_id) AS customer_amount
+FROM
+    customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON ci.city_id = a.city_id
+JOIN country co ON co.country_id = ci.country_id
+GROUP BY
+    city, country
+ORDER BY customer_amount DESC
+LIMIT 10;

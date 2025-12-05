7ab03c74fae3bb9337a943bada64549425366264
Lucas Lindh <lucas.lindh6@live.se>
2025-12-05 17:14:55 +0100

    Task 2 done

--- sql/eda_sakila.sql
+++ sql/eda_sakila.sql
@@ -143,6 +143,28 @@ LIMIT 10;
 -- Task 2
 -- a)
 SELECT
-    first_name || ' ' || last_name AS name
+    first_name || ' ' || last_name AS name,
+    SUM(p.amount) AS total_spent,
+    c.customer_id
 FROM
-    customer;
+    customer c
+JOIN payment p ON p.customer_id = c.customer_id
+GROUP BY name, c.customer_id
+ORDER BY total_spent DESC
+LIMIT 5;
+
+-- b)
+SELECT
+    c.name AS category,
+    SUM(p.amount) AS total_revenue
+FROM
+    category c
+JOIN film_category fc ON fc.category_id = c.category_id
+JOIN film f ON f.film_id = fc.film_id
+JOIN inventory i ON i.film_id = f.film_id
+JOIN rental r ON r.inventory_id = i.inventory_id
+JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY
+    c.name
+ORDER BY
+    total_revenue DESC;

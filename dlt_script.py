c04a7695ebebd7c30973925c0385f121d23f5896
Lucas Lindh <lucas.lindh6@live.se>
2025-12-11 14:29:25 +0100

    fixes before video

--- dlt_script.py
+++ dlt_script.py
@@ -2,6 +2,8 @@ import dlt
 from dlt.sources.sql_database import sql_database
 from pathlib import Path
 
+
+# Hittar filer
 # Här hittar vi data-mappen
 DATA_PATH = Path(__file__).parent / "data"
 
@@ -11,7 +13,7 @@ SQLITE_PATH = DATA_PATH / "sqlite-sakila.db"
 # Vart vi vill skapa DuckDB-filen
 DUCKDB_PATH = DATA_PATH / "sakila.duckdb"
 
-# Här skapar vi källan
+# Här skapar vi DLT-källan
 source = sql_database(credentials=f"sqlite:///{SQLITE_PATH}", schema="main")
 
 # Här skapar vi en pipeline, det bestämmer hur datan laddas
@@ -21,7 +23,7 @@ pipeline = dlt.pipeline(
         dataset_name="staging",
 )
 
-# Här ersätter vi all tidigare data
+# Här ersätter vi all tidigare data/kör pipelinen
 load_info = pipeline.run(source, write_disposition="replace")
 
 print(load_info)
